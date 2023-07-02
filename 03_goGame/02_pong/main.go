62d8708b82b172b76fb456e98fbaba58108c82d9
arron <fsxchen@gmail.com>
2023-07-02 09:10:38 +0800

    feat(pong): pong project init.

---
+++ 03_goGame/02_pong/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello world")
+
+	// capablity to render stuff on screen
+	// Draw paddles
+	// Playr movement
+	// Take care of paddle boundaries
+	// Draw ball
+	// Update ball movement
+	// Handle collisions
+	// Handle game over
+}
